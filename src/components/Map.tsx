e5f480970f0fadf8f8209cfdca6868cb7c381eca
AR <alexrevtovich@gmail.com>
2024-02-07 01:47:48 -0600

    implemented routes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -59,7 +59,7 @@ const Map: React.FC = () => {
     };
 
     fetchAndSetAddress();
-  }, []);
+  }, [myLocation]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
