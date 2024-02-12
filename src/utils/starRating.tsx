b7c6b888a215aa750dfb5661c17770cdef3165e2
AR <alexrevtovich@gmail.com>
2024-02-12 10:10:42 -0600

    shows half stars

--- src/utils/starRating.tsx
+++ src/utils/starRating.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faStarHalfAlt, faStar as farStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'; // Importing regular (empty) star icon
 
-// Define an interface for the component's props
 interface StarRatingProps {
   rating: number; // Specify that rating should be a number
 }
@@ -12,11 +12,11 @@ const StarRating: React.FC<StarRatingProps> = ({ rating }) => {
   let stars = [];
 
   for (let i = 1; i <= totalStars; i++) {
-    if (i <= rating) {
+    if (i <= Math.floor(rating)) {
       // Full star
       stars.push(<FontAwesomeIcon key={i} icon={faStar} />);
     } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
-      // Half star
+      // Half star, only added if the rating has a fractional part
       stars.push(<FontAwesomeIcon key={i} icon={faStarHalfAlt} />);
     } else {
       // Empty star
