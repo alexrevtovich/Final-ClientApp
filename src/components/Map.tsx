58f6382006d8a936533e85a6a31a0c4d64df9c39
AR <alexrevtovich@gmail.com>
2024-02-11 23:31:31 -0600

    logo

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -21,19 +21,21 @@ const Map: React.FC = () => {
   const [myLocation, setMyLocation] = useState<[number, number]>([29.7174, -95.4028]); //default value that is valid for sure
   const myLocationRef = useRef<[number, number]>(myLocation); // Ref to hold the current location
 
-  // Define updateMyLocation within the component
-  const updateMyLocation: React.Dispatch<React.SetStateAction<[number, number]>> = (value) => {
-    // If value is a function, call it with the current myLocation to get the new location
-    const newLocation = typeof value === 'function' ? value(myLocation) : value;
+  // Memoize updateMyLocation using useCallback
+  const updateMyLocation = useCallback((value: [number, number] | ((prevState: [number, number]) => [number, number])) => {
+    // Directly use setMyLocation since it's stable and doesn't change
+    setMyLocation((currentLocation) => {
+      const newLocation = typeof value === 'function' ? value(currentLocation) : value;
+
+      if (typeof newLocation[0] === 'number' && typeof newLocation[1] === 'number') {
+        return newLocation; // Return newLocation directly
+      } else {
+        console.error('Tried to set invalid myLocation:', newLocation);
+        return [29.7174, -95.4028]; // Return default location
+      }
+    });
+  }, []);
   
-    if (typeof newLocation[0] === 'number' && typeof newLocation[1] === 'number') {
-      setMyLocation(newLocation);
-    } else {
-      console.error('Tried to set invalid myLocation:', newLocation);
-      // Optionally, revert to a default location
-      setMyLocation([29.7174, -95.4028]);
-    }
-  };
 
   // Define fetchAndDisplayStations function using useCallback
   const fetchAndDisplayStations = useCallback(async (location: string) => {
@@ -62,9 +64,9 @@ const Map: React.FC = () => {
 
 
   // Fetch the user's location and set it to state
-useEffect(() => {
-  getMyLocation(updateMyLocation); // Use updateMyLocation to ensure validation
-}, [updateMyLocation]);
+  useEffect(() => {
+    getMyLocation(updateMyLocation); // Pass updateMyLocation to getMyLocation
+  }, [updateMyLocation]);
 
 
   // Redirect to login if user email is not found in session storage
