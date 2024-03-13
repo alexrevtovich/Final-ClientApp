e0a42a8470c640f9a611c6dcc5cd288277701785
alexrevtovich <alexrevtovich@gmail.com>
2024-03-13 14:27:04 -0500

    cosmetics

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,4 +1,3 @@
-// Account.tsx
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import fetchAccountInfo from '../utils/accountinfo';
@@ -7,14 +6,23 @@ import reverseGeocode from '../utils/reverse';
 import AddCar from '../utils/addCar';
 import fetchCarInfo from '../utils/carInfo';
 
+interface CarDetail {
+  brand: string;
+  model: string;
+  releaseYear: number;
+  charge?: number;
+}
+
 const Account: React.FC = () => {
   const navigate = useNavigate();
   const userEmail = sessionStorage.getItem("userEmail");
-  const [userInfo, setUserInfo] = useState({ username: '', email: '', mainCar: '' });
-  const [carInfo, setCarInfo] = useState<{ brand: string, model: string, releaseYear: number, charge: number } | null>(null);
+  const [userInfo, setUserInfo] = useState({ username: '', email: '', mainCar: '', cars: [] });
+  const [carInfo, setCarInfo] = useState<CarDetail | null>(null);
+  const [carsInfo, setCarsInfo] = useState<CarDetail[]>([]);
   const [myLocation, setMyLocation] = useState<[number, number]>([0, 0]);
   const [address, setAddress] = useState('');
   const [showModal, setShowModal] = useState(false);
+  const [showCarsList, setShowCarsList] = useState(false);
 
   useEffect(() => {
     if (!userEmail) {
@@ -24,19 +32,17 @@ const Account: React.FC = () => {
       (async () => {
         try {
           const accountInfo = await fetchAccountInfo(userEmail);
-          setUserInfo(accountInfo); // Adjusted to store the entire account info response
+          setUserInfo(accountInfo);
 
-          // Fetch car info based on user's main car unique ID
-          if (accountInfo.mainCar) {
-            const carInfo = await fetchCarInfo(accountInfo.mainCar);
-            setCarInfo(carInfo);
+          if (accountInfo.mainCar && accountInfo.mainCar !== "None") {
+            const mainCarInfo = await fetchCarInfo(accountInfo.mainCar);
+            setCarInfo(mainCarInfo);
           }
         } catch (error) {
           console.error('Failed to fetch account info:', error);
         }
       })();
     }
-
     getMyLocation(setMyLocation);
   }, [navigate, userEmail]);
 
@@ -49,6 +55,22 @@ const Account: React.FC = () => {
     })();
   }, [myLocation]);
 
+  useEffect(() => {
+    const fetchCarsInfo = async () => {
+      const carsDetailsPromises = userInfo.cars.map(carId => fetchCarInfo(carId));
+      try {
+        const carsDetails = await Promise.all(carsDetailsPromises);
+        setCarsInfo(carsDetails.filter(car => car !== null));
+      } catch (error) {
+        console.error('Error fetching cars details:', error);
+      }
+    };
+
+    if (userInfo.cars.length > 0) {
+      fetchCarsInfo();
+    }
+  }, [userInfo.cars]);
+
   const toggleModal = () => setShowModal(!showModal);
 
   const handleUpdateCar = () => {
@@ -56,19 +78,32 @@ const Account: React.FC = () => {
     // Logic to handle updating car information
   };
 
+  const toggleCarsList = () => setShowCarsList(!showCarsList);
+
   return (
     <div className="account-container">
       <h2>Account Page</h2>
       <div className="account-info">Hello {userInfo.username}</div>
       <div className="account-info">Your email is: {userInfo.email}</div>
-      <div className="account-info">Main Car ID: {userInfo.mainCar || "None"}</div>
-      <div className="account-info">
-        Your car is: {carInfo ? `${carInfo.brand} ${carInfo.model} (${carInfo.releaseYear}) - ${carInfo.charge}%` : 'No car info'} 
-        <button onClick={toggleModal}>Add Car</button>
-      </div>
       <div className="account-info">
         You are here: {address || 'Fetching your address...'}
       </div>
+      <div className="account-info">
+        Your car is: {carInfo ? `${carInfo.brand} ${carInfo.model} (${carInfo.releaseYear}) - ${carInfo.charge}%` : 'No car info'} 
+        
+      </div>
+      
+      <button onClick={toggleCarsList}>{showCarsList ? 'Hide' : 'Show'} My Garage</button>
+      {showCarsList && (
+        <div className="cars-list">
+          <ul>
+            {carsInfo.map((car, index) => (
+              <li key={index}>{`${car.brand} ${car.model} (${car.releaseYear})`}</li>
+            ))}
+          </ul>
+          <button onClick={toggleModal}>Add Car</button>
+        </div>
+      )}
       {showModal && (
         <div className="modal">
           <button className="close-modal" onClick={toggleModal}>X</button>
